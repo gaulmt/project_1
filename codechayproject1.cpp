dd042f19aab38a604ac3148aed55371ddcbfe957
Nguyễn Đăng Dương <lmtgau@gmail.com>
2026-01-28 14:24:47 +0700

    cập nhật 1 tí

--- codechayproject1.cpp
+++ codechayproject1.cpp
@@ -1,26 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//Tối đa của sách,chuỗi và số người dùng 
-#define MAX_BOOKS 1000
-#define MAX_USERS 100
-#define MAX_STRING 100
-/*project yêu cầu dùng typedef nên bê y nguyên vô code chay được.TRƯỚC KHI NỘP NHỚ XOÁ DÒNG NÀY*/
-//Định nghĩa struct book
-typedef struct Book {
-    int id;
-    char title[MAX_STRING];
-    char author[MAX_STRING];
-    int year;
-    char status[20]; // status gồm: Có sẵn & Đã mượn
-    struct Book* next;
-} Book;
-// Định nghĩa struct User(morong1)
-typedef struct {
-    int id;
-    char name[MAX_STRING];
-    char borrowed_books[10][MAX_STRING]; // Tối đa 10 sách
-    int num_borrowed;
-} User;
 
+// thiết kế menu
+void menu(){
+        printf("\n================= MENU =================\n");
+        printf("1.  Them sach moi\n");
+        printf("2.  Xoa danh sach\n");
+        printf("3.  Sua thong tin sach\n");
+        printf("4.  Tim kiem theo ten sach\n");
+        printf("5.  Tim kiem theo ten tac gia\n");
+        // Mở rộng / sáng tạo
+        printf("6.  Them nguoi muon\n");
+        printf("7.  Muon sach\n");
+        printf("8.  Tra sach\n");
+        printf("9.  Sap xep theo nam xuat ban\n");
+        printf("10. Luu du lieu\n");
+        printf("0.  Thoat\n");
+        printf("========================================\n");
+}
+// thêm sách
+int add_book(){
+    printf("vui long nhap id sach: ");
+    scanf("%d",&book[i].id);
+    printf("vui long nhap ten sach: ");
+    fgets(book[i].title,sizeof(book[i].id),stdin);
+    printf("vui long nhap ten tac gia: ");
+    fgets(book[i].author,sizeof(book[i].author),stdin);
+    printf("vui long nhap nam san xuat: ");
+    scanf("%d",&book[i].year);
+    scanf(1,book[i].status);
+}
+//xóa danh sách
+void del_book(){
+
+}
+// sửa thông tin sách
+int fix_inf(){
+    printf("nhap id sach can sua: ");
+    printf("nhap ten sach moi (neu doi): ");
+    printf("nhap ten tac gia moi(neu doi): ");
+    printf("nhap nam san xua moi: ");
+
+}
+// tìm kiếm theo tên
+int find_name(){
+
+}
+// tìm kiếm theo tác giả
+int find_author(){
+
+}
+// thêm người mượn
+int add_user(){
+
+}
+// mượn sách
+int muon_sach(){
+
+}
+// trả sách
+int tra_sach(){
+
+}
+// sắp xếp danh sách theo năm
+int sort_year(){
+
+}
+// lưu dữ liệu ra file
+void save(){
+
+}
+int main(){
+    // định nghĩa struct book
+    struct qltv{
+        int id,
+        char title[200],
+        char author[100],
+        int year,
+        int status,  
+                // 1: sách đang được mượn
+                // 0: sách chưa ai mượn
+    }
+    struct qltv sv[100];
+    // thiết lập các tùy chọn
+    int tc;
+    int i=0;
+    while (true){
+        menu();
+        printf("nhap tuy chon cua ban: ");
+        scanf("%d",&tc);
+        switch (tc)
+        {
+        case 1:
+            add_book(i);
+            break;
+        case 2:
+            del_book();
+            break;
+        case 3:
+            fix_inf();
+            break;
+        case 4:
+            find_name();
+            break;
+        case 5:
+            find_author();
+            break;
+        case 6:
+            add_user();
+            break;
+        case 7:
+            muon_sach();
+            break;
+        case 8:
+            tra_sach();
+            break;
+        case 9:
+            sort_year();
+            break;
+        case 10:
+            save();
+        default:
+            printf("cu phap sai vui long nhap lai!");
+            break;
+        }
+        i++;
+    }
+}
 
